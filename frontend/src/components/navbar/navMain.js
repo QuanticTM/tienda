af5ece30efc2cf115f07a67909b9edcb7bdcf7c4
Brayan S. Chavarro Leon <brayanchavarro081@gmail.com>
2021-10-22 10:38:04 -0500

    nav progress

--- frontend/src/components/navbar/navMain.js
+++ frontend/src/components/navbar/navMain.js
@@ -1,11 +1,27 @@
-
+import { useContext, useState } from "react";
+import { GlobalContext } from "../../logic/globalState";
+import BD from "./BD.svg";
+import NavMenu from "./navMenu";
 
 const Navbar = () => {
 
+    const { tema } = useContext(GlobalContext);
+    const [ isDisplayed, setDisplay] = useState(false);
+    
+    const st = {backgroundColor: tema.principal};
+    const st1 = {height: "25px"};
+    
+    const Logo = () => <img src={BD} className="logo" style={st1} />
+
     return ( 
-        <div className="navbar-main" >
-            <p>Death</p>
-        </div>
+        <>
+            <div className="navbar-main" style={st} >
+                <div onClick={() => setDisplay(!isDisplayed)} >
+                    <Logo />
+                </div>
+            </div>
+            {isDisplayed && <NavMenu> <p>Drogas</p></NavMenu> }
+        </>
     )
 };
 
---
+++ frontend/src/components/navbar/navMenu.js
@@ -0,0 +1,9 @@
+
+const NavMenu = props => {
+    
+    return  <div className="options-wrapper nav-menu" > {props.children} </div>
+}
+
+export default NavMenu;
+
+
