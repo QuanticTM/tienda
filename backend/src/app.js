9fd9a2e4a752cf96adcbaa705cc790b538ab6880
Brayan S. Chavarro Leon <brayanchavarro081@gmail.com>
2021-10-26 11:31:40 -0500

    mongo connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const exp = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const app = exp();
 
 // routes
@@ -15,6 +16,23 @@ app.set('port', process.env.PORT || 4269 );
 // middleware
 
 app.use(exp.urlencoded({extended: false}));
+
+const connectionUrl = "mongodb+srv://dbAdmin:b1pIzXu8ucc6RXt4@cluster0.5roux.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+
+const { MongoClient } = require('mongodb');
+
+try{
+    const client = new MongoClient(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+      client.close();
+    });
+}
+catch(err){
+    console.log(err);
+}
 
 
 app.listen( app.get("port"), () => {
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,14 @@
-const express = request("");
-const router = express.router();
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req,res) =>{
+    res.send("hello world");
+})
+
+router.get("/product/add", (req,res) =>{
+    console.log(req.body);
+    console.log("negros muy")
+})
 
 module.exports = router;
 
