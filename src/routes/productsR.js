ce78f60b25ecc201d91de31b66abc4ad6c6e821d
Brayan S. Chavarro Leon <brayanchavarro081@gmail.com>
2021-11-11 23:33:34 -0500

    other products routes

--- src/routes/productsR.js
+++ src/routes/productsR.js
@@ -3,7 +3,7 @@ const Router = express.Router();
 const Product = require("../blueprints/products");
 
 
-Router.post("/create", async (req,res) => {
+Router.post("/create", async (req, res) => {
     console.log(req.body);
     console.log(req.body.name);
     const product = new Product({
@@ -17,6 +17,28 @@ Router.post("/create", async (req,res) => {
 })
 
 
+Router.get("/findall", (req, res) => {
+    
+    console.log("pedidos todos")
+    Product.find({}, (err, result) => {
+        err ? console.log(err) :
+            res.json(result);
+    });
+    console.log("termino")
+})
+
+Router.post("/change-disponibility", async (req, res) => {
+   
+    try{
+        let productChoose = await Product.findById( req.body.id);
+        productChoose.disponibility = !productChoose.disponibility;
+        productChoose.save();
+        res.send("disponibilidad actualizada");
+    }
+    catch(err){
+        console.log(err);
+    }
+}) 
 
 
 module.exports = Router;
